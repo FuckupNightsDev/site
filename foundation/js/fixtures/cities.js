a26c16eba762e2ed51f767c152a03a2e256db8cc
Faridh Mendoza <faridh@intangible.mx>
2014-03-03 21:58:31 -0600

    Initial commit

---
+++ foundation/js/fixtures/cities.js
@@ -0,0 +1,14 @@
+var cities = [
+
+  { "id" : 1, "name" : "DF", "slug" : "df", "color" : "#000000" },
+  { "id" : 2, "name" : "Monterrey", "slug" : "monterrey", "color" : "#2C3E50" },
+  { "id" : 3, "name" : "Chihuahua", "slug" : "chihuahua", "color" : "#8E44AD" },
+  { "id" : 4, "name" : "Toluca", "slug" : "toluca", "color" : "#2980B9" },
+  { "id" : 5, "name" : "Hermosillo", "slug" : "hermosillo", "color" : "#27AE60" },
+  { "id" : 6, "name" : "Guadalajara", "slug" : "guadalajara", "color" : "#16A085" },
+  { "id" : 7, "name" : "Mérida", "slug" : "merida", "color" : "#F39C12" },
+  { "id" : 8, "name" : "San Luis Potosí", "slug" : "san_luis_potosi", "color" : "#D35400" },
+  { "id" : 9, "name" : "León", "slug" : "leon", "color" : "#C0392B" },
+  { "id" : 10, "name" : "Oaxaca", "slug" : "oaxaca", "color" : "#7F8C8D" }
+
+];
